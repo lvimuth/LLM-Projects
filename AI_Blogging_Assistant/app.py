59367bef6f4bd89c1822e8f40f5791347203dce9
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-01 22:56:43 +0400

    streamlit

---
+++ AI_Blogging_Assistant/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+#Title for the application
+st.title('📝 BlogCraft: Your AI Writing Companion 🤖')
+
+#Create a subheader
+st.subheader('Now you can craft perfect blogs with the help of AI - BlogCraft is your NEW AI BLOG Companion')
