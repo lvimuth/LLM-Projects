c406481b625435eeee16f72b1601ab6f92096eca
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-02 23:44:50 +0400

    update the application

--- AI_Assistant_Doctor_Patients/model.py
+++ AI_Assistant_Doctor_Patients/model.py
@@ -15,7 +15,7 @@ def upload_to_gemini(path, mime_type=None):
   See https://ai.google.dev/gemini-api/docs/prompting_with_media
   """
   file = genai.upload_file(path, mime_type=mime_type)
-  print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+#   print(f"Uploaded file '{file.display_name}' as: {file.uri}")
   return file
 
 # Create the model
